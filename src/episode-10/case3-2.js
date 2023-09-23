02a32800e96bc93160aa245fe2c142ff5a551bed
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 13:51:30 +0900

    3-ガード節による入れ子の条件記述の置き換え - Fix 一番上の条件判定でガード節を使用

--- src/episode-10/case3-2.js
+++ src/episode-10/case3-2.js
@@ -3,22 +3,22 @@
 
 // 従業員が会社に在籍している場合のみ支払う
 // isSeparated（離職者）とisRetired（退職者）を除外する必要がある
+
+// 主目的である 計算 は、2つの条件が当てはまらない従業員 に対してのみ適用されます。
+// このような場合、ガード節を 使用するとコードの意図 を明快に表現できる。
 function payAmount(employee) {
   let result;
-  if (employee.isSeparated) {
-    result = { amount: 0, reasonCode: "SEP" };
+  if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" };
+  if (employee.isRetired) {
+    result = { amount: 0, reasonCode: "RET" };
   } else {
-    if (employee.isRetired) {
-      result = { amount: 0, reasonCode: "RET" };
-    } else {
-      // 金額を計算するロジック
-      lorem.ipsum(dolor.sitAmet);
-      consectetur(adipiscing).elit();
-      setImmediate.do.eiusmod =
-        tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-      ut.enim.ad(ninim.veniam);
-      result = someFinalComputation;
-    }
+    // 金額を計算するロジック
+    lorem.ipsum(dolor.sitAmet);
+    consectetur(adipiscing).elit();
+    setImmediate.do.eiusmod =
+      tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+    ut.enim.ad(ninim.veniam);
+    result = someFinalComputation;
   }
   return result;
 }
