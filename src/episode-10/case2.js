51b85459c3baeab018c1493e0bb9146c13e32910
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 13:09:36 +0900

    2-条件記述の統合 - Fix or演算子を使って結合

--- src/episode-10/case2.js
+++ src/episode-10/case2.js
@@ -1,3 +1,9 @@
-if (anEmployee.seniority < 2) return 0;
-if (anEmployee.monthsDisabled > 12) return 0;
-if (anEmployee.isPartTime) return 0;
+// 障害手当てを計算する
+// 連続した条件判定ですが、結果は全て同じ。
+// 結果が同じなので、条件を１つにまとめるべき。
+// 連続した条件判定の場合はor演算子 || を使って結合します。
+
+function disabilityAmount(anEmployee) {
+  if (anEmployee.seniority < 2 || anEmployee.monthsDisabled > 12) return 0;
+  if (anEmployee.isPartTime) return 0;
+}
