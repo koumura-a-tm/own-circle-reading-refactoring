a0979f7e055522e4da489832ac0c84df3d1df7b6
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 16:33:15 +0900

    4-バリエーションに対してポリモーフィズムを適用する - Fix history.lengthで関数の抽出の適用

--- src/episode-10/case4-2.js
+++ src/episode-10/case4-2.js
@@ -51,9 +51,13 @@ class Rating {
 
   get voyageAndHistoryLengthFactor() {
     let result = 0;
-    if (this.history.length > 8) result += 1;
+    result += this.historyLengthFactor;
     if (this.history.length > 14) result -= 1;
     return result;
+  }
+
+  get historyLengthFactor() {
+    return this.history.length > 8 ? 1 : 0;
   }
 
   get hasChina() {
@@ -71,10 +75,14 @@ class ExperiencedChinaRating extends Rating {
   get voyageAndHistoryLengthFactor() {
     let result = 0;
     result += 3;
-    if (this.history.length > 10) result += 1;
+    result += this.historyLengthFactor;
     if (this.history.length > 12) result += 1;
     if (this.history.length > 18) result -= 1;
     return result;
+  }
+
+  get historyLengthFactor() {
+    return this.history.length > 10 ? 1 : 0;
   }
 }
 
