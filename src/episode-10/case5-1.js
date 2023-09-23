8e6a43b6a8ce1cc3ef73b2cf78c56d66d9a58f2f
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 17:08:48 +0900

    5-特殊ケースの導入 - Add 公共事業者のサービス

---
+++ src/episode-10/case5-1.js
@@ -0,0 +1,36 @@
+// ある公共事業会社が、多くのSite（場所）にサービスを提供しています。
+class Site {
+  get customer() {
+    return this._customer;
+  }
+}
+
+// Customer（顧客）クラスには様々なプロパティがあります。
+class Customer {
+  get name() {}
+  get billingPlan() {}
+  set billingPlan(arg) {}
+  get paymentHistory() {}
+}
+
+// client1
+const aCustomer = site.customer;
+// ... 大量のコードが入る ...
+let customerName;
+if (aCustomer === "unknown") customerName = "occupant";
+else customerName = aCustomer.name;
+
+// client2
+const plan =
+  aCustomer === "unknown"
+    ? CustomElementRegistry.billingPlans.basic
+    : aCustomer.billingPlan;
+
+// client3
+if (aCustomer !== "unknown") aCustomer.billingPlan = newPlan;
+
+// client4
+const weeksDelinquent =
+  aCustomer === "unknown"
+    ? 0
+    : aCustomer.paymentHistory.weeksDelinquentInLastYear;
