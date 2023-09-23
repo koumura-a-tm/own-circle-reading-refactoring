616d0a84c19ce6610565ef83c3ffb1d5e4cdb1e9
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 13:35:04 +0900

    3-ガード節による入れ子の条件記述の置き換え - Fix getPayAmount

--- src/episode-10/case3.js
+++ src/episode-10/case3.js
@@ -1,12 +1,6 @@
 function getPayAmount() {
-  let result;
-  if (isDead) result = deadAmount();
-  else {
-    if (isSeparated) result = separatedAmount();
-    else {
-      if (isRetired) result = retiredAmount();
-      elseresult = normalPayAmount();
-    }
-  }
-  return result;
+  if (isDead) return deadAmount();
+  if (isSeparated) return separatedAmount();
+  if (isRetired) return retiredAmount();
+  return normalPayAmount();
 }
