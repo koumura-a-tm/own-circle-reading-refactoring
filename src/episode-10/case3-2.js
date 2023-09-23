2319b7762c810178ca26ee95dd9a5f6e1998b741
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 13:53:18 +0900

    3-ガード節による入れ子の条件記述の置き換え - Fix 次の条件判定でガード節を使用

--- src/episode-10/case3-2.js
+++ src/episode-10/case3-2.js
@@ -9,16 +9,13 @@
 function payAmount(employee) {
   let result;
   if (employee.isSeparated) return { amount: 0, reasonCode: "SEP" };
-  if (employee.isRetired) {
-    result = { amount: 0, reasonCode: "RET" };
-  } else {
-    // 金額を計算するロジック
-    lorem.ipsum(dolor.sitAmet);
-    consectetur(adipiscing).elit();
-    setImmediate.do.eiusmod =
-      tempor.incididunt.ut(labore) && dolore(magna.aliqua);
-    ut.enim.ad(ninim.veniam);
-    result = someFinalComputation;
-  }
+  if (employee.isRetired) return { amount: 0, reasonCode: "RET" };
+  // 金額を計算するロジック
+  lorem.ipsum(dolor.sitAmet);
+  consectetur(adipiscing).elit();
+  setImmediate.do.eiusmod =
+    tempor.incididunt.ut(labore) && dolore(magna.aliqua);
+  ut.enim.ad(ninim.veniam);
+  result = someFinalComputation;
   return result;
 }
