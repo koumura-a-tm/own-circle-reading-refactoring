b0517621b928bb1cde04dc15811c2ade67c90bba
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 14:32:06 +0900

    4-ポリモーフィズムによる条件記述の置き換え - Fix ポリモーフィズムによる条件記述の置き換え

--- src/episode-10/case4.js
+++ src/episode-10/case4.js
@@ -1,11 +1,18 @@
-// 鳥の種類に基づいてその個体の状態を評価し、文字列として返す switch文
-switch (bird.type) {
-  case "EuropeanSwallow":
+// ポリモーフィズムによる条件記述の置き換え
+class EuropeanSwallow {
+  get plumage() {
     return "avarage";
-  case "AfircanSwallow":
-    return bird.numberOfCoconuts > 2 ? "tired" : "avarage";
-  case "NorwegianBlueParrot":
-    return bird.voltage > 100 ? "scorched" : "beautiful";
-  default:
-    return "unknown";
+  }
+}
+
+class AfircanSwallow {
+  get plumage() {
+    return this.numberOfCoconuts > 2 ? "tired" : "avarage";
+  }
+}
+
+class NorwegianBlueParrot {
+  get plumage() {
+    return this.voltage > 100 ? "scorched" : "beautiful";
+  }
 }
