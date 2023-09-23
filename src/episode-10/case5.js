5999e1ed09a0de5b80aa9cd793f0dabe8b96ddc9
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 16:58:50 +0900

    5-特殊ケースの導入 - Fix 特殊ケースの導入 修正

--- src/episode-10/case5.js
+++ src/episode-10/case5.js
@@ -1,3 +1,8 @@
 // 特殊ケースの導入
-// aCustomer という変数の値が 厳密な等価で unnknown の場合、customerName に occupant を代入する。
-if (aCustomer === "unnknown") customerName = "occupant";
+
+// 特殊ケース用のクラス を作成して置き換え
+class UnknownCustomer {
+  get name() {
+    return "occupant";
+  }
+}
