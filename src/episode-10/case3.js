4c9ce20b4226ee202ccff56cab926d5ef89d6465
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 13:34:21 +0900

    3-ガード節による入れ子の条件記述の置き換え - Add getPayAmount

---
+++ src/episode-10/case3.js
@@ -0,0 +1,12 @@
+function getPayAmount() {
+  let result;
+  if (isDead) result = deadAmount();
+  else {
+    if (isSeparated) result = separatedAmount();
+    else {
+      if (isRetired) result = retiredAmount();
+      elseresult = normalPayAmount();
+    }
+  }
+  return result;
+}
