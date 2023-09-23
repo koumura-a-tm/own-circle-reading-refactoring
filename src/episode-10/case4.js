463140f865e2accadfd4e71c8141cd4dce32596c
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-09-23 14:24:31 +0900

    4-ポリモーフィズムによる条件記述の置き換え - Add 鳥の種類のswitch文

---
+++ src/episode-10/case4.js
@@ -0,0 +1,11 @@
+// 鳥の種類に基づいてその個体の状態を評価し、文字列として返す switch文
+switch (bird.type) {
+  case "EuropeanSwallow":
+    return "avarage";
+  case "AfircanSwallow":
+    return bird.numberOfCocounts > 2 ? "tired" : "avarage";
+  case "NorwegianBlueParrot":
+    return bird.voltage > 100 ? "scorched" : "beautiful";
+  default:
+    return "unknown";
+}
