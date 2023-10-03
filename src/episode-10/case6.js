fed7ecb65ffa7049e920cb121b6b6a2e5b995b34
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-03 11:28:34 +0900

    Fix 6 - add assert

--- src/episode-10/case6.js
+++ src/episode-10/case6.js
@@ -2,6 +2,9 @@
 class Customer {
   applyDiscount(aNumber) {
     if (!this.discountRate) return aNumber;
-    else return aNumber - this.discountRate * aNumber;
+    else {
+      assert(this.discountRate >= 0);
+      return aNumber - this.discountRate * aNumber;
+    }
   }
 }
