e3efcd2e5ab8beaeac09227b8fdf6a53f3840d12
幸村 昌裕 <komura.masahiro@a-tm.co.jp>
2023-10-03 11:27:17 +0900

    Fix 6 - change if-then statement

--- src/episode-10/case6.js
+++ src/episode-10/case6.js
@@ -1,6 +1,7 @@
 // アサーションの導入
 class Customer {
   applyDiscount(aNumber) {
-    return this.discountRate ? aNumber - this.discountRate * aNumber : aNumber;
+    if (!this.discountRate) return aNumber;
+    else return aNumber - this.discountRate * aNumber;
   }
 }
